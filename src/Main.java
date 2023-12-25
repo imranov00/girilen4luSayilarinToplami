7549e18b2d212cba4c4a1ded24481864ee5bd68f
İmranov <semtexpert@gmail.com>
2023-12-25 15:12:25 +0300

    Versiyon 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n;
+        int toplam = 0;
+
+
+
+        Scanner inp = new Scanner(System.in);
+        do {
+            System.out.println("Sayı Giriniz: ");
+            n= inp.nextInt();
+            if (n  %2  ==0 || n % 2 == 0 ){
+                toplam += n;
+
+
+            }
+
+
+        }while (n % 2 == 0 || n % 4 == 0);
+        System.out.print("Girilen Çift ve 4'ün katları olan sayıların toplamı: ");
+        System.out.println(toplam);
+
     }
 }
